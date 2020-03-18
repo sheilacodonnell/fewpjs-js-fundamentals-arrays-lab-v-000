1a6fdbae45ff5bd80524f624d7b21e1c027db623
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-18 21:13:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const append = ["Milo", "Otis", "Garfield"]
-const prepent = ["Milo", "Otis", "Garfield"]
+const prepend = ["Milo", "Otis", "Garfield"]
 const removeLast = ["Milo", "Otis", "Garfield"]
 const removeFirst = ["Milo", "Otis", "Garfield"]
 
 append.push("Odie")
+prepend.unshift("Odie")
